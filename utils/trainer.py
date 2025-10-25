e5bd4d069fb126a5b9f8597ae4235f19f1c137e9
DaoChaShao <27882272@qq.com>
2025-10-25 21:36:19 +0800

    Initial commit

---
+++ utils/trainer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/25 18:43
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   trainer.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+if __name__ == "__main__":
+    main()
