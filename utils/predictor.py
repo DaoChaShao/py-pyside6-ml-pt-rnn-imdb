74318f03ee0ce1bced6526efc1df08489ed9b677
DaoChaShao <27882272@qq.com>
2025-10-31 11:50:48 +0800

    feat: delete the predictor.py because the time of the io implementation is quite long for M1 Apple Chip MacBook

--- utils/predictor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3.12
-# -*- Coding: UTF-8 -*-
-# @Time     :   2025/10/25 18:41
-# @Author   :   Shawn
-# @Version  :   Version 0.1.0
-# @File     :   predictor.py
-# @Desc     :   
-
-
